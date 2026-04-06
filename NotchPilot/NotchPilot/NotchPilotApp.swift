997e3dcec2ca8f826f35807fa66c12eb6c82a802
candroid <cnrture@gmail.com>
2026-04-07 02:53:09 +0300

    Fix embedded git repo issue

---
+++ NotchPilot/NotchPilot/NotchPilotApp.swift
@@ -0,0 +1,17 @@
+//
+//  NotchPilotApp.swift
+//  NotchPilot
+//
+//  Created by Caner on 7.04.2026.
+//
+
+import SwiftUI
+
+@main
+struct NotchPilotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
